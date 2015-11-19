8529b3f3678745fe827cb702635d92888e3f8d36
Eric Hollenberry <erichollenberry@gmail.com>
2015-11-18 20:54:12 -0800

    (feat) - Added Trend Line

--- public/src/app.js
+++ public/src/app.js
@@ -7,7 +7,6 @@ app.controller('appCtrl', function($scope, $http) {
   // * show meta data as an object (reponse from AJAX call?)
   $scope.results = [];
   // * d3 object / data set (when data is changed page is update)
-
   // * search function
   $scope.submit = function() {
     // - make call to AJAX factory
@@ -15,23 +14,22 @@ app.controller('appCtrl', function($scope, $http) {
     var season = 1;
     var seasonExists = true;
     var getAllSeasons = function(seasonNumber) {
-    	$http({
-    		//need to handle url spaces
-    		method: 'GET',
+      $http({
+        //need to handle url spaces
+        method: 'GET',
         params: {t: $scope.query, type: 'series', season: seasonNumber},
-    		url: 'http://www.omdbapi.com/?',
-    	}).then(function(res) {
+        url: 'http://www.omdbapi.com/?',
+      }).then(function(res) {
         console.log(res);
         if (res.data.Response === "True") {
           $scope.results = res.data;
           getAllSeasons(seasonNumber + 1);
         }
-    		//run d3 function with data
-    	}, function(err) {
+        //run d3 function with data
+      }, function(err) {
         console.log(err);
       });
     };
     getAllSeasons(season);
   };
 });
-
